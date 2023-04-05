a2b6dbf696601f668eba472971aeb9be778f53db
LYK <dalinaum@gmail.com>
2023-04-06 00:45:59 +0900

    searchedRepositories에서 캐쉬된 리포지토리 검색 결과를 전달함.
    - searchedRepositories를 통해 캐시된 리포지토리 검색 결고를 전달.
    - Paging 3을 통해 깃헙 검색.
    - 불필요한 searchRepositories 함수는 제거하고 MainScreen에서 호출하는 파트도 제거.

--- app/src/main/java/io/github/dalinaum/m_issues/viewModel/GithubViewModel.kt
+++ app/src/main/java/io/github/dalinaum/m_issues/viewModel/GithubViewModel.kt
@@ -3,9 +3,11 @@ package io.github.dalinaum.m_issues.viewModel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.dalinaum.m_issues.api.GithubService
-import kotlinx.coroutines.launch
+import io.github.dalinaum.m_issues.data.Item
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
@@ -13,10 +15,45 @@ import javax.inject.Inject
 class GithubViewModel @Inject constructor(
     private val githubService: GithubService
 ) : ViewModel() {
-    fun searchRepositories() {
-        viewModelScope.launch {
-            val result = githubService.searchRepositories("dalinaum")
-            Log.d("!!!", result.toString())
+    val searchedRepositories = getRepositoriesFlow("D").cachedIn(viewModelScope)
+
+    private val pageSize = 30
+
+    private fun getRepositoriesFlow(query: String): Flow<PagingData<Item>> = Pager(
+        config = PagingConfig(
+            pageSize = pageSize,
+            enablePlaceholders = true,
+            prefetchDistance = 10
+        ),
+        initialKey = 1
+    ) {
+        object : PagingSource<Int, Item>() {
+            override fun getRefreshKey(state: PagingState<Int, Item>): Int? {
+                return state.anchorPosition?.let { anchorPosition ->
+                    state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                        ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+                }
+            }
+
+            override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Item> {
+                return try {
+                    val response = githubService.searchRepositories(
+                        searchQuery = query,
+                        page = params.key,
+                        perPage = pageSize
+                    )
+
+                    LoadResult.Page(
+                        data = response.items,
+                        prevKey = if (params.key == 1) null else params.key?.minus(1),
+                        nextKey = if (response.items.isEmpty()) null else params.key?.plus(1)
+                    )
+                } catch (e: Exception) {
+                    Log.e("EEE", "error: $e")
+                    e.printStackTrace()
+                    LoadResult.Error(e)
+                }
+            }
         }
-    }
+    }.flow
 }
