c7b6ee2cfd62ac62925dd4daaf144ec316b9fca9
LYK <dalinaum@gmail.com>
2023-04-06 03:10:48 +0900

    검색 키워드를 ResultScreen으로 전달하여 표시
    - Scaffold로 TopAppBar 추가
    - 결과 화면에 업네비게이션 등록

--- app/src/main/java/io/github/dalinaum/m_issues/ui/ResultScreen.kt
+++ app/src/main/java/io/github/dalinaum/m_issues/ui/ResultScreen.kt
@@ -1,10 +1,13 @@
 package io.github.dalinaum.m_issues.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,35 +20,62 @@ import io.github.dalinaum.m_issues.viewModel.GithubViewModel
 
 @Composable
 fun ResultScreen(
+    query: String,
     navController: NavController,
     viewModel: GithubViewModel = hiltViewModel(),
 ) {
+    viewModel.setQuery(query)
     val lazyPagingItems = viewModel.searchedRepositories.collectAsLazyPagingItems()
-    LazyColumn {
-        if (lazyPagingItems.loadState.refresh == LoadState.Loading) {
-            item {
-                Text(
-                    text = "Waiting for items to load from the backend",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                )
-            }
-        }
-        items(lazyPagingItems) { item ->
-            if (item != null) {
-                Text(item.name)
-            } else {
-                Text("가져오는 중입니다.")
-            }
-        }
-        if (lazyPagingItems.loadState.append == LoadState.Loading) {
-            item {
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.navigateUp()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "뒤로 가기"
+                        )
+                    }
+                },
+                title = {
+                    Text(text = "Result: $query")
+                })
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
+        Surface(
+            modifier = Modifier.padding(paddingValues.calculateBottomPadding())
+        ) {
+            LazyColumn {
+                if (lazyPagingItems.loadState.refresh == LoadState.Loading) {
+                    item {
+                        Text(
+                            text = "Waiting for items to load from the backend",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentWidth(Alignment.CenterHorizontally)
+                        )
+                    }
+                }
+                items(lazyPagingItems) { item ->
+                    if (item != null) {
+                        Text(item.name)
+                    } else {
+                        Text("가져오는 중입니다.")
+                    }
+                }
+                if (lazyPagingItems.loadState.append == LoadState.Loading) {
+                    item {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentWidth(Alignment.CenterHorizontally)
+                        )
+                    }
+                }
             }
         }
     }
