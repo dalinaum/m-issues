0a126c4a2e1972f75094d4ddd432fabf8c7d232f
LYK <dalinaum@gmail.com>
2023-04-06 00:48:02 +0900

    ResultScreen에서 러프하게 결과를 보여줌.
    - name만 보여주도록 구성.
    - 로딩 중의 멘트와 추가의 멘트도 러프하게 구성.

--- app/src/main/java/io/github/dalinaum/m_issues/ui/ResultScreen.kt
+++ app/src/main/java/io/github/dalinaum/m_issues/ui/ResultScreen.kt
@@ -1,9 +1,18 @@
 package io.github.dalinaum.m_issues.ui
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
 import io.github.dalinaum.m_issues.viewModel.GithubViewModel
 
 @Composable
@@ -11,5 +20,31 @@ fun ResultScreen(
     navController: NavController,
     viewModel: GithubViewModel = hiltViewModel(),
 ) {
-    Text("Result")
+    val lazyPagingItems = viewModel.searchedRepositories.collectAsLazyPagingItems()
+    LazyColumn {
+        if (lazyPagingItems.loadState.refresh == LoadState.Loading) {
+            item {
+                Text(
+                    text = "Waiting for items to load from the backend",
+                    modifier = Modifier.fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                )
+            }
+        }
+        items(lazyPagingItems, key = { it.id }) { item ->
+            if (item != null) {
+                Text(item.name)
+            } else {
+                Text("가져오는 중입니다.")
+            }
+        }
+        if (lazyPagingItems.loadState.append == LoadState.Loading) {
+            item {
+                CircularProgressIndicator(
+                    modifier = Modifier.fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                )
+            }
+        }
+    }
 }
