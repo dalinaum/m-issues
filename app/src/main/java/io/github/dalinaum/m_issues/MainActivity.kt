1d18f9ac6d653e0ddaac647907bfa4d987ccd8a0
LYK <dalinaum@gmail.com>
2023-04-05 16:21:33 +0900

    가장 기본적인 네비게이션 설정을 추가
    - NavHostController를 등록.
    - 임시로 Home과 Result를 추가

--- app/src/main/java/io/github/dalinaum/m_issues/MainActivity.kt
+++ app/src/main/java/io/github/dalinaum/m_issues/MainActivity.kt
@@ -1,16 +1,23 @@
 package io.github.dalinaum.m_issues
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dagger.hilt.EntryPoint
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.dalinaum.m_issues.ui.theme.MIssuesTheme
 
@@ -25,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TopLevel()
                 }
             }
         }
@@ -33,14 +40,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopLevel(
+    navHostController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navHostController,
+        "Home",
+        modifier = modifier
+    ) {
+        composable("Home") {
+            Column {
+                Text("Hello")
+                Button(onClick = {
+                    navHostController.navigate("Result")
+                }) {
+                    Text("이동")
+                }
+            }
+        }
+        composable("Result") {
+            Text("Result")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MIssuesTheme {
-        Greeting("Android")
+        TopLevel()
     }
 }
